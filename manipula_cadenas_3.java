9cf92e66c5d1681029cded0a73ff557133f2ee73
Gallogit91 <80711213+Gallogit91@users.noreply.github.com>
2023-04-06 00:44:19 +0200

    Add files via upload

---
+++ manipula_cadenas_3.java
@@ -0,0 +1,26 @@
+package com.ipartek;
+
+public class manipula_cadenas_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String alumno1, alumno2;
+		
+		alumno1="David";
+		alumno2="david";
+		
+		System.out.println(alumno1.equalsIgnoreCase(alumno2));
+		
+		
+String alumno3, alumno4;
+		
+		alumno3="David";
+		alumno4="david";
+		
+		System.out.println(alumno3.equals(alumno4));
+		
+		
+	}
+
+}
